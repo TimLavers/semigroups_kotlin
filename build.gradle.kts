c2d53166201197282c1520d8f1b071df472baaa1
Tim Lavers <laverstim@gmail.com>
2019-12-31 10:15:37 +1100

    Use Gradle for build.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.61" 
+}
+val compileKotlin: KotlinCompile by tasks
+val compileTestKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+repositories {
+    jcenter() 
+}
+
+dependencies {
+    implementation(kotlin("stdlib")) 
+	testImplementation("junit:junit:4.12")
+}
