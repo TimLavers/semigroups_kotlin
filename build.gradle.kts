d4a297b9a049cbbe18fd2260f54f3d5ad6e7f5c6
Tim Lavers <laverstim@gmail.com>
2023-06-25 18:47:08 +1000

    Update version of Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.61" 
+    kotlin("jvm") version "1.8.21"
 }
 val compileKotlin: KotlinCompile by tasks
 val compileTestKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
 repositories {
     mavenCentral()
 }
